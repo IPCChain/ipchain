2a828e612469bf7c29f1f780b0dc75c2a190a667
summerxue1992 <xxy+940048700@qq.com>
2018-11-29 10:44:41 +0800

    add method "GetIndexKey"

--- src/base58.cpp
+++ src/base58.cpp
@@ -292,6 +292,25 @@ bool CBitcoinAddress::IsScript() const
     return IsValid() && vchVersion == Params().Base58Prefix(CChainParams::SCRIPT_ADDRESS);
 }
 
+bool CBitcoinAddress::GetIndexKey(uint160& hashBytes, int& type) const
+{
+	if (!IsValid()) {
+		return false;
+	}
+	else if (vchVersion == Params().Base58Prefix(CChainParams::PUBKEY_ADDRESS)) {
+		memcpy(&hashBytes, &vchData[0], 20);
+		type = 1;
+		return true;
+	}
+	else if (vchVersion == Params().Base58Prefix(CChainParams::SCRIPT_ADDRESS)) {
+		memcpy(&hashBytes, &vchData[0], 20);
+		type = 2;
+		return true;
+	}
+
+	return false;
+}
+
 void CBitcoinSecret::SetKey(const CKey& vchSecret)
 {
     assert(vchSecret.IsValid());
